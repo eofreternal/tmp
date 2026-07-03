fedd7ab3fb33f96cb24b44074dcaf18c196f2f00
Eternal <chasezou09@gmail.com>
2026-07-03 19:19:56 -0400

    Reset go brrrr

---
+++ src/styles/global.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const colors = {
+    background: "#0f0f0f",
+
+
+}
+
+export const globalStyles = StyleSheet.create({
+    view: {
+        flex: 1,
+        backgroundColor: colors.background,
+        paddingTop: 60,
+    }
+}) 
