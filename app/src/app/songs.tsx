25cfb1e7a1be6f4503f2024aac0a489543ebe196
Eternal <chasezou09@gmail.com>
2026-07-04 16:39:02 -0400

    misc(backend): forgot to add backend

--- app/src/app/songs.tsx
+++ app/src/app/songs.tsx
@@ -3,7 +3,7 @@ import { globalStyles } from "@/styles/global"
 
 export default function IndexScreen() {
     return (
-        <ScrollView style={globalStyles.view}>
+        <ScrollView style={[globalStyles.view, globalStyles.safeAreaPadding]}>
             <Text style={globalStyles.text}>Songssssssssssssssssssss</Text>
             <Text style={globalStyles.text}>{Platform.OS}</Text>
         </ScrollView>
