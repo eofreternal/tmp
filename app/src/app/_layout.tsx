c82e9eb71cd81c1d634c071a0d87dcb2fa13d9b6
Eternal <chasezou09@gmail.com>
2026-07-07 09:11:56 -0400

    refactor(app): process all songs at the same time

--- app/src/app/_layout.tsx
+++ app/src/app/_layout.tsx
@@ -13,53 +13,59 @@ import { getAudioMetadata } from '@missingcore/audio-metadata';
 import { eq } from "drizzle-orm"
 import useMusic from "@/state/music"
 
+const wantedTags = ['album', 'albumArtist', 'artist', 'name', 'track', 'year', "artwork"] as const;
+async function handleFile(file: FileSystem.File) {
+  console.log(file.name)
+  if ([".m4a", ".mp3"].includes(file.extension) === false) {
+    return
+  }
+
+  const [song] = await db.select().from(schema.songsTable).where(eq(schema.songsTable.name, i.name))
+  if (song === undefined) {
+    const metadata = await getAudioMetadata(file.uri, wantedTags)
+    const coverArt = handleCoverArt(file.name, metadata.metadata.artwork)
+    console.log(coverArt)
+    await db.insert(schema.songsTable).values({
+      name: file.name,
+      uri: file.uri,
+      coverArtUri: coverArt,
+
+      lastModified: new Date(file.lastModified!)
+    })
+  } else {
+    if (song.lastModified.getTime() !== file.lastModified!) {
+      const metadata = await getAudioMetadata(file.uri, wantedTags)
+      const coverArt = handleCoverArt(file.name, metadata.metadata.artwork)
+      await db.update(schema.songsTable).set({
+        name: file.name,
+        uri: file.uri,
+        coverArtUri: coverArt,
+
+        lastModified: new Date(file.lastModified!)
+      }).where(eq(schema.songsTable.name, file.name))
+    }
+  }
+}
+
 export default function RootLayout() {
   const musicState = useMusic((state) => state)
   useMigrations(db, migrations);
-  const wantedTags = ['album', 'albumArtist', 'artist', 'name', 'track', 'year', "artwork"] as const;
 
   useEffect(() => {
     async function main() {
       //TODO: clean up old songs that aren't in the music folder anymroe
+      const process = []
       const uri = await getOrRequestMusicFolder()
-      console.log("yeah")
       const directory = new FileSystem.Directory(uri)
       const list = directory.list()
       for (const i of list) {
         if (i instanceof FileSystem.File) {
-          console.log(i.extension)
-          if ([".m4a", ".mp3"].includes(i.extension) === false) {
-            continue
-          }
-
-          const [song] = await db.select().from(schema.songsTable).where(eq(schema.songsTable.name, i.name))
-          if (song === undefined) {
-            console.log(`insert ${i.name}`)
-            const metadata = await getAudioMetadata(i.uri, wantedTags)
-            const coverArt = handleCoverArt(i.name, metadata.metadata.artwork)
-            console.log(coverArt)
-            await db.insert(schema.songsTable).values({
-              name: i.name,
-              uri: i.uri,
-              coverArtUri: coverArt,
-
-              lastModified: new Date(i.lastModified!)
-            })
-          } else {
-            if (song.lastModified.getTime() !== i.lastModified!) {
-              const metadata = await getAudioMetadata(i.uri, wantedTags)
-              const coverArt = handleCoverArt(i.name, metadata.metadata.artwork)
-              await db.update(schema.songsTable).set({
-                name: i.name,
-                uri: i.uri,
-                coverArtUri: coverArt,
-
-                lastModified: new Date(i.lastModified!)
-              }).where(eq(schema.songsTable.name, i.name))
-            }
-          }
+          process.push(handleFile(i))
         }
       }
+
+      await Promise.allSettled(process)
+
       console.log("all songs")
       const allSongs = await db.select().from(schema.songsTable)
       console.log(allSongs)
