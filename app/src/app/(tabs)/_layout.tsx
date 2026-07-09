40201aa3dd4a4d4f07b0fcaa581b55f8f692da4e
Eternal <chasezou09@gmail.com>
2026-07-09 12:22:08 -0400

    fix(app): who swapped the icons?

--- app/src/app/(tabs)/_layout.tsx
+++ app/src/app/(tabs)/_layout.tsx
@@ -26,11 +26,11 @@ export default function RootLayout() {
             }}>
                 <Tabs.Screen name="index" options={{
                     title: "Home",
-                    tabBarIcon: ({ color, size }) => (<Ionicons name="musical-note" size={size} color={color} />)
+                    tabBarIcon: ({ color, size }) => (<Entypo name="home" size={size} color={color} />)
                 }} />
                 <Tabs.Screen name="songs" options={{
                     title: "Songs",
-                    tabBarIcon: ({ color, size }) => (<Entypo name="home" size={size} color={color} />)
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="musical-note" size={size} color={color} />)
                 }} />
             </Tabs>
         </>
