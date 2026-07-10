c038e462de6094df3bc1a15c0304cb30c2d62169
Eternal <chasezou09@gmail.com>
2026-07-10 16:55:46 -0400

    feat(app): playlist support in the db go brrr

--- app/src/db/index.ts
+++ app/src/db/index.ts
@@ -1,9 +1,10 @@
 import { drizzle } from "drizzle-orm/expo-sqlite";
 import { openDatabaseSync } from "expo-sqlite";
+import { relations } from "./schema"
 
 const expo = openDatabaseSync("songs.sqlite");
 expo.execSync("PRAGMA journal_mode = WAL;");
 
-const db = drizzle(expo);
+const db = drizzle(expo, { relations: relations });
 
 export { db }
--- app/src/db/schema.ts
+++ app/src/db/schema.ts
@@ -1,4 +1,5 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
+import { defineRelations } from 'drizzle-orm';
 
 export const songsTable = sqliteTable("songs", {
     id: integer().primaryKey({ autoIncrement: true }),
@@ -10,3 +11,31 @@ export const songsTable = sqliteTable("songs", {
 
     lastModified: integer({ mode: "timestamp_ms" }).notNull()
 })
+
+export const playlistTable = sqliteTable("playlists", {
+    id: integer().primaryKey({ autoIncrement: true }),
+    name: text().notNull(),
+})
+
+export const playlistSongsJunctionTable = sqliteTable("playlistSongsJunction", {
+    playlistId: integer().notNull().references(() => playlistTable.id, { onDelete: "cascade" }),
+    songId: integer().notNull().references(() => songsTable.id, { onDelete: "cascade" }),
+
+    order: integer().notNull()
+})
+
+export const relations = defineRelations({ songsTable, playlistTable, playlistSongsJunctionTable }, (r) => ({
+    songsTable: {
+        playlists: r.many.playlistTable({
+            from: r.songsTable.id.through(r.playlistSongsJunctionTable.playlistId),
+            to: r.playlistTable.id.through(r.playlistSongsJunctionTable.playlistId)
+        })
+    },
+
+    playlistSongsJunctionTable: {
+        songData: r.one.songsTable({
+            from: r.playlistSongsJunctionTable.songId,
+            to: r.songsTable.id
+        })
+    }
+}))
