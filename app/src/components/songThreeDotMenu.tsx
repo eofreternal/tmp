acd447d1a8ffb20394e5223b84c712f12546ed8f
Eternal <chasezou09@gmail.com>
2026-07-31 18:26:45 -0400

    style(app): songThreeDotMenu is prettier now :D

--- app/src/components/songThreeDotMenu.tsx
+++ app/src/components/songThreeDotMenu.tsx
@@ -1,7 +1,12 @@
-import { BottomSheet, Button } from "@expo/ui"
+import { BottomSheet, Button, Column, Text, Spacer } from "@expo/ui"
 import AddToPlaylist from "./addToPlaylist"
 import { useState } from "react"
 import useMusicStore, { Song } from "@/state/music"
+
+import MaterialDesignIcons from "@react-native-vector-icons/material-design-icons"
+
+import * as Modifer from "@expo/ui/jetpack-compose/modifiers"
+import { colors } from "@/styles/global"
 
 export default function SongThreeDotMenu({ show, songId, onClose }: { show: boolean, songId: number | undefined, onClose: () => void }) {
     const musicState = useMusicStore((state) => state)
@@ -11,23 +16,34 @@ export default function SongThreeDotMenu({ show, songId, onClose }: { show: bool
     return (
         <>
             <BottomSheet isPresented={show} onDismiss={() => onClose()}>
-                <Button onPress={async () => {
-                    if (songId === undefined) {
-                        //TODO: throw and error
-                        return
-                    }
-                    await musicState.addSongToQueue(songId)
-                }} label="Add to queue" />
+                <Column>
+                    <Button onPress={async () => {
+                        if (songId === undefined) {
+                            //TODO: throw and error
+                            return
+                        }
+                        await musicState.addSongToQueue(songId)
+                    }} modifiers={[Modifer.fillMaxWidth()]} variant="text">
+                        <MaterialDesignIcons name="playlist-music" size={24} color={colors.light} />
+                        <Spacer size={16} />
+                        <Text textStyle={{ textAlign: "left" }} modifiers={[Modifer.fillMaxWidth()]}>Add to queue</Text>
+                    </Button>
 
-                <Button onPress={() => {
-                    if (songId === undefined) {
-                        //TODO: throw and error
-                        return
-                    }
-                    setShowPlaylists(true)
-                }} label="Add to playlist" />
+                    <Button onPress={() => {
+                        if (songId === undefined) {
+                            //TODO: throw and error
+                            return
+                        }
+                        setShowPlaylists(true)
+                    }} modifiers={[Modifer.fillMaxWidth()]} variant="text" >
+                        <MaterialDesignIcons name="playlist-plus" size={24} color={colors.light} />
+                        <Spacer size={16} />
+                        <Text textStyle={{ textAlign: "left" }} modifiers={[Modifer.fillMaxWidth()]}>Add to playlist</Text>
+                    </Button>
+                </Column>
             </BottomSheet>
 
             <AddToPlaylist show={showPlaylists} songId={songId} onClose={() => setShowPlaylists(false)} />
-        </>)
+        </>
+    )
 }
